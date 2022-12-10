d0103196b989ce4e6971491355b58b8bc681f9f7
thienle77 <tle9677@gmail.com>
2022-12-10 17:03:38 -0500

    dice class made

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,19 @@
+import java.util.Random;
+
 public class Dice {
+    int numDice;
+    int sides = 6;
 
+    public Dice(int numDice) {
+        this.numDice = numDice;
+    }
 
+    public int rollDice (){
+        Random random = new Random();
+        int sum = 0;
+        for(int i = 0; i < numDice; i++){
+            sum += random.nextInt(sides)+ 1;
+        }
+        return sum;
+    }
 }
